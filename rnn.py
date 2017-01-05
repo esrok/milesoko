6d0db75f713f95ccbe856f33ae2ed6b4769e74e3
korse <korse@seclab.cs.msu.su>
2017-01-05 22:31:07 +0300

    Add simple script for 1-layer RNN training
    Input sequences length and neurons count can be specified. Models are
    saved with weights, so training can be resumed later.

---
+++ rnn.py
@@ -0,0 +1,242 @@
+from glob import glob
+import json
+import os.path
+import random
+
+from keras.callbacks import Callback, ModelCheckpoint
+from keras.layers import Activation, Dense, Dropout, LSTM
+from keras.models import Sequential, model_from_json
+import numpy as np
+
+
+import logging
+
+logger = None
+def init_logging():
+    global logger
+
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+init_logging()
+
+
+def get_char_indicies(data):
+    chars = sorted(list(set(data)))
+    char_to_index = {
+        c: i for i, c in enumerate(chars)
+    }
+    index_to_char = {
+        i: c for i, c in enumerate(chars)
+    }
+    return char_to_index, index_to_char
+
+
+def get_train_data(data, seq_length, step=1):
+    char_to_index, index_to_char = get_char_indicies(data)
+    n_chars = len(char_to_index)
+
+    seqs = []
+    next_char = []
+    for offset in xrange(0, len(data) - seq_length, step):
+        seqs.append(data[offset:offset + seq_length])
+        next_char.append(data[offset + seq_length])
+    n_samples = len(seqs)
+
+    X = np.zeros((n_samples, seq_length, n_chars), dtype=np.bool)
+    y = np.zeros((n_samples, n_chars), dtype=np.bool)
+
+    for i, seq in enumerate(seqs):
+        for char_index, char in enumerate(seq):
+            X[i, char_index, char_to_index[char]] = 1
+        y[i, char_to_index[next_char[i]]] = 1
+
+    return X, y
+
+
+class GenerationCallback(Callback):
+    SAMPLE_PATTERN = 'samples-{epoch:02d}-{diversity:.1f}.txt'
+    DIVERSITIES = (0.2, 0.5, 0.7, 1.0, 1.2)
+
+    def __init__(self, wrapper, output_dir, sample_length=None, sample_pattern=None):
+        self._wrapper = wrapper
+        self.sample_pattern = self.SAMPLE_PATTERN
+        self.output_dir = output_dir
+
+        if sample_pattern:
+            self.sample_pattern = sample_pattern
+
+        self.sample_length = self._wrapper._input_length * 20
+        if sample_length is not None:
+            self.sample_length = sample_length
+        super(GenerationCallback, self).__init__()
+
+    def on_epoch_end(self, epoch, logs=None):
+        if logs is None:
+            logs = {}
+
+        seed = self._wrapper.get_random_seed()
+        for d in self.DIVERSITIES:
+            filename = os.path.join(self.output_dir, self.sample_pattern.format(
+                epoch=epoch,
+                diversity=d,
+                **logs
+            ))
+            with open(filename, 'w') as f:
+                logger.debug('Storing samples in %s', filename)
+                f.write(self._wrapper.generate(
+                    seed=seed,
+                    length=self.sample_length,
+                    diversity=d,
+                ).encode('utf-8'))
+
+
+
+class RNNWrapper(object):
+    MODEL_FILENAME = 'model.json'
+    WRAPPER_FILENAME = 'wrapper.json'
+
+    def __init__(self, data, output_dim, input_length, layers=1, dropout=None, output_dir=None, sample_length=None, initial_epoch=0):
+        self._initial_epoch = initial_epoch
+        self._data = data
+        self.char_to_index, self.index_to_char = get_char_indicies(data)
+
+        self._input_dim = len(self.char_to_index)
+        logger.debug('Input dim %d', self._input_dim)
+        self._input_length = input_length
+
+        self._model = self._create(output_dim, self._input_dim, input_length, layers, dropout)
+        self._callbacks = []
+        if output_dir is not None:
+            self._callbacks.append(GenerationCallback(self, output_dir, sample_length=sample_length))
+            self._callbacks.append(self.get_save_callback(output_dir))
+
+            path = os.path.join(output_dir, self.MODEL_FILENAME)
+            logger.debug('Storing model in %s', path)
+            with open(path, 'w') as f:
+                f.write(self._model.to_json())
+
+            path = os.path.join(output_dir, self.WRAPPER_FILENAME)
+            logger.debug('Storing wrapper in %s', path)
+            with open(path, 'w') as f:
+                f.write(json.dumps({
+                    'output_dim': output_dim,
+                    'input_length': input_length,
+                    'layers': layers,
+                    'dropout': dropout,
+                    'output_dir': output_dir,
+                }))
+        self._fit = False
+
+    WEIGHTS_FILENAME_PATTERN = 'weights-{epoch:02d}-{loss:.4f}.hdf5'
+    WEIGHTS_FILENAME_GLOB = 'weights-*-*.hdf5'
+
+    @classmethod
+    def get_save_callback(cls, dirname, model_name=None):
+        pattern = str(cls.WEIGHTS_FILENAME_PATTERN)
+        if model_name is not None:
+            pattern = model_name + '-' + pattern
+        callback = ModelCheckpoint(
+            os.path.join(dirname, pattern),
+            monitor='loss', verbose=1, save_best_only=True, mode='min',
+        )
+        return callback
+
+    @classmethod
+    def get_best_model(cls, data, from_dir):
+        model_filename = os.path.join(from_dir, cls.MODEL_FILENAME)
+        wrapper_filename = os.path.join(from_dir, cls.WRAPPER_FILENAME)
+        weights_filenames = glob(os.path.join(from_dir, cls.WEIGHTS_FILENAME_GLOB))
+        assert os.path.exists(model_filename), \
+            '%s must exist to load from dir' % cls.MODEL_FILENAME
+        assert os.path.exists(wrapper_filename), \
+            '%s must exist to load from dir' % cls.WRAPPER_FILENAME
+
+        best_epoch = None
+        best_loss = None
+        best_weights = None
+
+        for weights_filename in weights_filenames:
+            _, epoch, loss = weights_filename[:-5].split('-')
+            epoch = int(epoch)
+            loss = float(loss)
+            if best_epoch is None or best_loss > loss:
+                best_epoch = epoch
+                best_loss = loss
+                best_weights = weights_filename
+        if best_epoch is not None:
+            logger.debug('Best epoch %d with loss %.4f at file %s', best_epoch + 1, best_loss, best_weights)
+        else:
+            logger.debug('No weights files were found')
+
+        with open(wrapper_filename, 'r') as f:
+            wrapper_params = json.load(f)
+
+        if best_epoch is not None:
+            # in keras logs epochs are 1-based
+            # and in callbacks epochs are 0-based
+            # so for epoch N in logs, filename is epoch - 1
+            initial_epoch = best_epoch + 1
+        else:
+            initial_epoch = None
+
+        wrapper = cls(data=data, initial_epoch=initial_epoch, **wrapper_params)
+        if best_weights is not None:
+            wrapper._model.load_weights(best_weights)
+        return wrapper
+
+    def _create(self, output_dim, input_dim, input_length, layers=1, dropout=None, ):
+        model = Sequential()
+        for i in xrange(layers):
+            if dropout is not None and dropout > 0:
+                model.add(Dropout(dropout))
+            if i == 0:
+                # input layer
+                model.add(LSTM(output_dim, input_dim=input_dim, input_length=input_length, ))
+        model.add(Dense(input_dim,))
+        model.add(Activation('softmax'))
+        model.compile(loss='categorical_crossentropy', optimizer='adam')
+        return model
+
+    def fit(self, nb_epoch=0, ):
+        self._fit = True
+        if self._initial_epoch > 1:
+            nb_epoch = nb_epoch + self._initial_epoch
+        X, y = get_train_data(self._data, self._input_length, )
+        self._model.fit(X, y, batch_size=128, nb_epoch=nb_epoch, callbacks=self._callbacks, initial_epoch=self._initial_epoch)
+
+    def sample(self, preds, diversity=1.0):
+        # helper function to sample an index from a probability array
+        preds = np.asarray(preds).astype('float64')
+        preds = np.log(preds) / diversity
+        exp_preds = np.exp(preds)
+        preds = exp_preds / np.sum(exp_preds)
+        probas = np.random.multinomial(1, preds, 1)
+        return np.argmax(probas)
+
+    def get_random_seed(self, length=None):
+        if length is None:
+            length = self._input_length
+        index = random.randint(0, len(self._data) - length)
+        return self._data[index:index + length]
+
+    def generate(self, seed=None, length=1, diversity=1.0):
+        assert self._fit
+
+        if seed is None:
+            seed = self.get_random_seed()
+        initial_seed = unicode(seed)
+        result = []
+        for _ in xrange(length):
+            seq = np.zeros((1, self._input_length, self._input_dim))
+            for i, char in enumerate(seed):
+                seq[0, i, self.char_to_index[char]] = 1
+
+            preds = self._model.predict(seq, )[0]
+            new_char_index = self.sample(preds, diversity)
+            new_char = self.index_to_char[new_char_index]
+            result.append(new_char)
+            seed += new_char
+            seed = seed[1:]
+        return initial_seed + ''.join(result)
---
+++ runner.py
@@ -0,0 +1,43 @@
+import argparse
+import os.path
+
+from rnn import RNNWrapper
+
+
+def is_dir(dirname):
+    """Checks if a path is an actual directory"""
+    if not os.path.isdir(dirname):
+        msg = "{0} is not a directory".format(dirname)
+        raise argparse.ArgumentTypeError(msg)
+    else:
+        return dirname
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--num', '-n', type=int, help='Number of neurons on LSTM layer.', )
+parser.add_argument('--length', '-l', type=int, help='Length of training sequences', )
+parser.add_argument('--epoch', '-e', type=int, help='Number of epochs to train', default=1)
+parser.add_argument('--layers', type=int, help='Number of LSTM layers.', default=1)
+parser.add_argument('--dropout', '-d', type=float, help='Dropout argument', default=0)
+parser.add_argument('--out', '-o', type=is_dir)
+parser.add_argument('--file', '-f', type=argparse.FileType('r'), help='data file', required=True)
+parser.add_argument('--resume', '-r', action='store_true', help='Resume training from dir', )
+parser.add_argument('--sample', '-s', type=int, help='Sample length', )
+
+args = parser.parse_args()
+print(args)
+
+data = args.file.read().decode('utf-8').lower().replace('\n', '')
+
+if not args.resume:
+    wrapper = RNNWrapper(data,
+        output_dim=args.num,
+        input_length=args.length,
+        layers=args.layers,
+        dropout=args.dropout,
+        output_dir=args.out,
+        sample_length=args.sample,
+    )
+else:
+    wrapper = RNNWrapper.get_best_model(data, args.out)
+wrapper.fit(nb_epoch=args.epoch)
